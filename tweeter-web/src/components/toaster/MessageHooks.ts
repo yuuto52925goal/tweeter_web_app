d7ad1e60845f73703686d941fb575433083e04fc
yuuto52925goal <yuuto52925goal@gmail.com>
2026-01-29 11:01:50 -0700

    Create Message hook

---
+++ tweeter-web/src/components/toaster/MessageHooks.ts
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { ToastActionsContext } from "./ToastContexts";
+import { ToastType } from "./Toast";
+
+interface MessageActions {
+  displayInfoMessage: (message: string, duration: number, bootstrapClasses?: string) => void;
+  displayErrorMessage: (message: string, bootstrapClasses?: string) => void;
+ deleteMessage: (messageId: string) => void;
+ deleteAllMessages: () => void;
+}
+
+export const useMessageActions = (): MessageActions => {
+  const { displayToast, deleteToast, deleteAllToasts } = useContext(ToastActionsContext);
+
+  return { 
+    displayInfoMessage: (message: string, duration: number, bootstrapClasses?: string) =>  displayToast(ToastType.Info, message, duration, undefined, bootstrapClasses), 
+    displayErrorMessage: (message: string, bootstrapClasses?: string) =>  displayToast(ToastType.Error, message, 0, bootstrapClasses),
+    deleteMessage: (messageId: string) =>  deleteToast(messageId),
+    deleteAllMessages: () =>  deleteAllToasts()
+  };
+};
