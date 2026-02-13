a989645a23f10f6c0ed824781a5eeb328ff5ea51
yuuto52925goal <yuuto52925goal@gmail.com>
2026-02-13 01:40:23 -0700

    Fix to public

--- tweeter-web/src/model.service/FollowService.ts
+++ tweeter-web/src/model.service/FollowService.ts
@@ -24,4 +24,24 @@ export class FollowService {
         return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
       };
 
+      public async loadMoreFollowees (
+        authToken: AuthToken,
+        userAlias: string,
+        pageSize: number,
+        lastItem: User | null
+      ): Promise<[User[], boolean]> {
+        // TODO: Replace with the result of calling server
+        return FakeData.instance.getPageOfUsers(lastItem, pageSize, userAlias);
+      };
+
+      public async loadMoreFollowers (
+        authToken: AuthToken,
+        userAlias: string,
+        pageSize: number,
+        lastItem: User | null
+      ): Promise<[User[], boolean]> {
+        // TODO: Replace with the result of calling server
+        return FakeData.instance.getPageOfUsers(lastItem, pageSize, userAlias);
+      };
+
 }
